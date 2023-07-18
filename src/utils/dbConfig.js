e97a3a8577318b1ffeed2eb7eaa681631f53bf73
Anideep <anideepkalia@gmail.com>
2023-07-18 20:42:22 +0530

    build for hosting

--- src/utils/dbConfig.js
+++ src/utils/dbConfig.js
@@ -4,13 +4,13 @@ import firebase from "firebase";
 // TODO: Add SDKs for Firebase products that you want to use
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD36HBqycqthQBBdtJ5slTWj_jm8ytyvck",
-  authDomain: "pleet-d8210.firebaseapp.com",
-  projectId: "pleet-d8210",
-  storageBucket: "pleet-d8210.appspot.com",
-  messagingSenderId: "752205409866",
-  appId: "1:752205409866:web:bbd0fccd1c79b7a2b500b3",
-  // measurementId: "G-T9113J49KY"
+  apiKey: "AIzaSyBxDmjFv-x52W7MrhAlc69ELq-asnyUyUk",
+  authDomain: "pleet-3555b.firebaseapp.com",
+  projectId: "pleet-3555b",
+  storageBucket: "pleet-3555b.appspot.com",
+  messagingSenderId: "247262048336",
+  appId: "1:247262048336:web:0118d03e853d2f15406155",
+  measurementId: "G-RJZNQ1EW89"
 };
 
 // Initialize Firebase
